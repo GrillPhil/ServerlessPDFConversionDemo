3fa56ab1e6df239b9241f3bb11a1e2001a7cc31a
Philipp Bauknecht <bauknecht@medialesson.de>
2020-05-01 18:56:46 +0200

    Cleaned up usings

--- ServerlessPDFConversionDemo/ConvertToPdf.cs
+++ ServerlessPDFConversionDemo/ConvertToPdf.cs
@@ -1,14 +1,10 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
 
 namespace ServerlessPDFConversionDemo
 {
--- ServerlessPDFConversionDemo/Startup.cs
+++ ServerlessPDFConversionDemo/Startup.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
